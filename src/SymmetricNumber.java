21106863c00450cf9c585e2ef9afcc767cee3a44
Daniyal <nurjanovdanioyal@gmail.com>
2024-10-14 14:17:46 +0500

    Some Correctives

--- src/SymmetricNumber.java
+++ src/SymmetricNumber.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SymmetricNumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.printf("Введите трехзначное число: ");
+        System.out.print("Введите трехзначное число: ");
         int num = in.nextInt();
         int firstDigit = num / 100;
         int lastDigit = num % 10;
